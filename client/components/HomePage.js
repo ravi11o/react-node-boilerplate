fe9522a5c09b33ac2ff0b92c99c461b91a8a9483
ravi11o <ravi.suraj110@gmail.com>
2021-04-10 10:53:49 +0530

    modified metadata file

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -5,6 +5,8 @@ const HomePage = () => {
   return (
     <div>
       <h1>React HomePage</h1>
+      <h3>This is a demo node react boilerplate with redux</h3>
+      <p>You can get the source code for this project at <a href="https://github.com/ravi11o/react-node-boilerplate">Github</a></p>
     </div>
   );
 }
