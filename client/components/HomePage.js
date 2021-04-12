e048e07e60d90d2a4b466542b673ec7e8a86d454
ravi11o <ravi.suraj110@gmail.com>
2021-04-12 12:09:29 +0530

    style changes

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -5,8 +5,8 @@ const HomePage = () => {
   return (
     <div>
       <h1>React HomePage</h1>
-      <h3>This is a demo node react boilerplate with redux</h3>
-      <p>You can get the source code for this project at <a href="https://github.com/ravi11o/react-node-boilerplate">Github</a></p>
+      <h3>This is a demo node, react boilerplate with redux as store</h3>
+      <p>Source code for this project is at <a target="_blank" href="https://github.com/ravi11o/react-node-boilerplate">Github</a></p>
     </div>
   );
 }
